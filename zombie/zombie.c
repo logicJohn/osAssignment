49200be04389ba104efc52aa2533219304f597a2
logicjohn <jay@jay-destop-18.04>
2019-02-24 20:42:33 -0500

    added comments and expaneded on readme.md

--- zombie/zombie.c
+++ zombie/zombie.c
@@ -1,24 +1,24 @@
+// Johnnie Hernandez & Malik Hill
+//  CSCI 3000
+
+// Program creates a child process the becomes a zombie process.
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	pid_t cPID = fork();
 
-	if(cPID < 0)
-	{
+	if(cPID < 0) {
 		perror("fork() error");
 		exit(-1);
 	} 
-	if(cPID != 0)
-	{
+	if(cPID != 0)	{
 		printf("I'm the parent proces: %d, The child process is: %d\n",getpid(),cPID);
 		sleep(10);
 		printf("Child: %d, was a zombie\n", cPID);
-	}
-	else{
+	}	else {
 		printf("I'm the child %d, my parent is %d\n",getpid(), getppid());
 		exit(0);
 	}
